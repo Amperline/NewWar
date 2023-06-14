a3529f2e348d54191f188c3d0e78d7d31a61c5b1
Amperline <artemonviktorenko@gmail.com>
2023-06-14 18:50:41 +0300

    Добавьте файлы проекта.

---
+++ NewWar_Server/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include <cppconn/prepared_statement.h>
+#include <cppconn/resultset.h>
+#include <SFML/Graphics.hpp>
+
+
+int main()
+{
+    
+
+
+
+
+	std::cin.get();
+	system("pause");
+	return 0;
+}
